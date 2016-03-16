d9c0e467e70114e3bba4c219acc825d083373625
leonshting <leonshting@gmail.com>
2016-03-16 16:28:07 +0300

    haskellwayadded

--- buffon/buffon_anotherway.hs
+++ buffon/buffon_anotherway.hs
@@ -1,29 +1,37 @@
 
 import System.Random
+import Data.Fixed
 
-closest :: Integral a => a -> a -> a
+freq = 4.0
+lengthi = 1.0
+numof = 100000
+
+kX::(Double, Double, Double, Double) -> Double
+kX (x1, x2, y1, y2) = (y2-y1)/(x2-x1)
+
+closest :: Double -> Double -> Double
 closest xc freq =
-        if (xc 'mod' freq) > freq/2
-        then freq - xc 'mod' freq
-        else xc 'mod' freq
+        if (xc `mod'` freq) > freq/2
+        then freq - xc `mod'` freq
+        else xc `mod'` freq
 
-ifintersect :: Integral a, Bool b => (a, a) -> a -> b
-ifintersect (x1,x2) freq =
-    if ((closest x1 freq) - abs x2 ) > 0
+ifintersect :: (Double, Double, Double, Double) -> Double -> Bool
+ifintersect (x1,x2, x3, x4) freq =
+    if ((closest x1 freq)^2 + (kX (x1,x2,x3,x4) * (closest x1 freq))^2 ) > lengthi^2
     then False
     else True
 
-to_iter :: Bool b, Int a => b ->(a, a) -> (a, a)
-to_iter b (a,c) =
+to_iter :: Bool -> Int -> Int
+to_iter b a =
     if b
-    then (a+1, c+1)
-    else (a, c+1)
+    then a+1
+    else a
 
 
-process ::(Int, Int) -> Double
-process (a,b) = a/b
+process :: Int -> Int -> Double
+process a b = 2*2*lengthi/freq/((fromIntegral a)/(fromIntegral b))
 
 main = do
 
-    (to_iter (ifintersect $ 0.1) (0,0)) take 10000 [(i,j)| i<-randomRs ((-1.0, 1.0) :: (Double, Double)) (mkStdGen 41))| j<-randomRs ((-50.0, 50.0) :: (Double, Double)) (mkStdGen 42))]
+    putStrLn (show (process (foldr (\x y ->to_iter (ifintersect x (freq :: Double)) y) 0 (take numof [(i,j,k,l)| i<-randomRs ((-50.0, 50.0) :: (Double, Double)) (mkStdGen 12)| j<-randomRs ((-50.0, 50.0) :: (Double, Double)) (mkStdGen 43)| k<-randomRs ((-50.0, 50.0) :: (Double, Double)) (mkStdGen 45)| l<-randomRs ((-50.0, 50.0) :: (Double, Double)) (mkStdGen 47)])) numof))
 
