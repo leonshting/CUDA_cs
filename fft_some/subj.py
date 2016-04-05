d6384118772f43876f0505ee5587e39218d1f4ab
Leonid Shtanko <leonshting@github.com>
2016-04-05 12:38:18 +0300

    done

--- fft_some/subj.py
+++ fft_some/subj.py
@@ -1,21 +1,64 @@
 __author__ = 'marusy'
 
-from scipy.fftpack import fft, ifft, rfft
+
 import numpy as np
 import matplotlib.pyplot as plt
-
+import random
+import math
+import cmath
 
 def f(x):
     return int(np.abs(10-x))
-x = np.arange(0, 50, 0.1)
-y = [f(X) for X in x]
-plt.plot(x, y)
 
+
+pi2 = cmath.pi * 2.0
+def DFT(fnList):
+    N = len(fnList)
+    FmList = []
+    for m in range(N):
+        Fm = 0.0
+        for n in range(N):
+            Fm += fnList[n] * cmath.exp(- 1j * pi2 * m * n / N)
+        FmList.append(Fm / N)
+    return FmList
+
+def InverseDFT(FmList):
+    N = len(FmList)
+    fnList = []
+    for n in range(N):
+        fn = 0.0
+        for m in range(N):
+            fn += FmList[m] * cmath.exp(1j * pi2 * m * n / N)
+        fnList.append(fn)
+    return fnList
+
+
+N = 100
+a = 2.0
+f = 2.0
+p = float(random.randint(0, 360))
+print "frequency = " + str(f)
+print "amplitude = " + str(a)
+print "phase ang = " + str(p)
+T = 1
+X = []
+freqs = []
+fnList = []
+for n in range(N):
+    t = float(n) / N * pi2
+    fn = a * math.sin(f * t + p / 360 * pi2) + math.sin(2*f * t + p / 360 * pi2)
+    fnList.append(fn)
+    X.append(f * t + p / 360 * pi2)
+    freqs.append(n/T)
+
+
+
+plt.plot(X, [i.real for i in fnList])
+plt.show()
+FmList = DFT(fnList)
+plt.plot(freqs,[i.real for i in FmList])
+plt.plot(freqs,[i.imag for i in FmList])
+plt.show()
+plt.plot(X, [i.real for i in InverseDFT(FmList)])
 plt.show()
 
-fftrans = fft(y, 128)
-plt.hist(np.real(fftrans), 100, facecolor='blue')
-plt.hist(np.imag(fftrans), 100, facecolor='red')
-plt.show()
-plt.plot(np.real(ifft(fftrans)))
-plt.show()
